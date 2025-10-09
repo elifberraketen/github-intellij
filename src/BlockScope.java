ccb763d19d15e438feb1ddc61bb3a550c04b130e
elifberraketen <ketenelifberra@gmail.com>
2025-10-09 10:46:59 +0300

    update

---
+++ src/BlockScope.java
@@ -0,0 +1,18 @@
+public class BlockScope {
+
+
+        static void main(String[] args) {
+
+            int outerX = 5;
+
+            if (true) {
+                int innerY = 15;
+
+                System.out.println("X inside the if block :" + outerX);
+                System.out.println("Y inside the if block :" + innerY);
+            }
+            System.out.println("X outside the if block :" + outerX);
+            //System.out.println("Y outside the if block :" +innerY); // compile time error
+
+        }
+}
